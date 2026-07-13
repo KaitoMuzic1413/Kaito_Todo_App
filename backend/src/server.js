35520a63a12632d1b22029632eb5c6ddae5853f2
Kaito <kaitomuzicvn@gmail.com>
2026-07-13 10:36:09 +0700

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,13 @@ import userRoutes from "./routes/userRoutes.js"; // 👈 1. Import thêm routes 
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from 'cors';
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 const app = express();
+const __dirname = path.resolve();
 
 // --- Middlewares ---
 
@@ -17,11 +19,21 @@ app.use(express.json());
 // 👈 2. Thêm dòng này giống như trong video (hỗ trợ đọc dữ liệu dạng form urlencoded)
 app.use(express.urlencoded({ extended: false })); 
 
-app.use(cors({ origin: ["http://localhost:5173"] }));
+if(process.env.NODE_ENV !== 'production'){
+    app.use(cors({ origin: ["http://localhost:5173"] }));
+}
 
 // --- Routes ---
 
 app.use("/api/tasks", taskRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
 
 // 👈 3. Thêm cổng định tuyến cho các tính năng Login/Register của User
 app.use("/api/users", userRoutes); 
